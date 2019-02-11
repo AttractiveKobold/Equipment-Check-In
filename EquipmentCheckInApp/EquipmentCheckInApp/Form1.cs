17d98bcb1e814d070a1b723c4bf2a46e5362345e
Jacob Mangia <tucker2134@gmail.com>
2019-02-10 23:22:22 -0500

    sunday Update
    I forgot to commit these changes and frankly I forget what they changed. This is bad form and I would never do this on a professional project.

--- EquipmentCheckInApp/EquipmentCheckInApp/Form1.cs
+++ EquipmentCheckInApp/EquipmentCheckInApp/Form1.cs
@@ -7,14 +7,65 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.OleDb;
 
 namespace EquipmentCheckInApp
 {
     public partial class Form1 : Form
     {
+        static OleDbConnection con;
+        static OleDbCommand cmd;
+        static OleDbDataReader reader;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+       
+        }
+
+        private void getAllEquipment()
+        {
+            //This is just an example of how to connect C# to the database and will not be in the final product
+            int counter = 0;
+            con = new OleDbConnection();
+            con.ConnectionString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=EquipmentDatabase.accdb";
+            cmd = new OleDbCommand();
+            cmd.Connection = con;
+            cmd.CommandText = "SELECT * FROM Equipment";
+            con.Open();
+            reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                counter++;
+                Console.WriteLine(reader[0] + " " + reader[1] + " " + reader[2]);
+            }
+            con.Close();
+            MessageBox.Show(counter.ToString());
+        }
+
+        private void updateHammer()
+        {
+            //This is just an example of how to connect C# to the database and will not be in the final product
+            con = new OleDbConnection();
+            con.ConnectionString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=EquipmentDatabase.accdb";
+            cmd = new OleDbCommand();
+            cmd.Connection = con;
+            cmd.CommandText = "UPDATE Equipment SET Equipment.[Checked Out To] = \"1\" WHERE(((Equipment.[ID]) = \"154845\"))";
+            con.Open();
+            int sonuc = cmd.ExecuteNonQuery();
+            con.Close();
+            if (sonuc > 0)
+            {
+                Console.WriteLine("Success");
+            }
+            else
+            {
+                Console.WriteLine("Failure");
+            }
         }
     }
 }
